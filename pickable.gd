3df07e1c146bf6ba9ec7bd1a640fc8fc21bacfbc
AntohaRDNS <antonardns@gmail.com>
2026-01-24 18:33:35 +0300

    add pickable script

--- pickable.gd
+++ pickable.gd
@@ -1,8 +1,19 @@
 class_name Pickable
 extends StaticBody3D
 
+@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var material: Material = (func() -> Material: return mesh_instance_3d.get_surface_override_material(0)).call()
 
 func pick()-> void:
 	print("pick")
-	$MeshInstance3D.get_surface_override_material(0).set("albedo_color", Color.RED)
+	mesh_instance_3d.get_surface_override_material(0).set("albedo_color", Color.RED)
+	pass
+	
+	
+func on_hover() -> void:
+	material.set("emission_energy_multiplier", 1.0)
+	pass
+	
+func on_unhover() -> void:
+	material.set("emission_energy_multiplier", 0.0)
 	pass
