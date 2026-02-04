836566166f650386a4e4147fc43911ca0da919f2
AntohaRDNS <antonardns@gmail.com>
2026-02-04 19:17:08 +0300

    tooth selection

--- Interactable/Interactable.gd
+++ Interactable/Interactable.gd
@@ -9,7 +9,9 @@ extends StaticBody3D
 
 var tween: Tween
 
-const PICK_TIME: float = 0.1
+const PICK_TIME: float = 0.
+
+var param_name: StringName = "emission_energy_multiplier"
 
 
 func pick()-> void:
@@ -23,9 +25,9 @@ func on_hover() -> void:
 	if tween != null:
 		tween.stop()
 		tween = null
-	
+		
 	tween = get_tree().create_tween()
-	tween.tween_method(func(value): material.set("emission_energy_multiplier", value), material.get("emission_energy_multiplier"), 1.0, PICK_TIME).set_ease(Tween.EASE_IN) # tween from current value to 1
+	tween.tween_method(func(value): mesh_instance_3d.set_instance_shader_parameter(param_name, value), mesh_instance_3d.get_instance_shader_parameter(param_name), 1.0, PICK_TIME).set_ease(Tween.EASE_IN) # tween from current value to 1
 	
 	audio_stream_player.play()
 	
@@ -39,5 +41,5 @@ func on_unhover() -> void:
 		tween = null
 		
 	tween = get_tree().create_tween()
-	tween.tween_method(func(value): material.set("emission_energy_multiplier", value), material.get("emission_energy_multiplier"), 0.0, PICK_TIME).set_ease(Tween.EASE_IN) # tween from current value to 0
+	tween.tween_method(func(value): mesh_instance_3d.set_instance_shader_parameter(param_name, value), mesh_instance_3d.get_instance_shader_parameter(param_name), 0.0, PICK_TIME).set_ease(Tween.EASE_IN) # tween from current value to 0
 	pass
