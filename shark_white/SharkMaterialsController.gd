1dd8d68c1760d3afac0c4cb04199977f184c00a1
AntohaRDNS <antonardns@gmail.com>
2026-02-10 08:06:48 +0300

    shark open mouth

--- shark_white/SharkMaterialsController.gd
+++ shark_white/SharkMaterialsController.gd
@@ -1,33 +1,44 @@
-@tool
 class_name SharkMaterialsController
 extends Node3D
 
 
 @export var body: MeshInstance3D
 @export var animation_tree: AnimationTree
+@onready var doctor: Node3D = $"../Doctor"
 
 var tween: Tween
 
 const GROW_TIME: float = 1.0
 
 
+func _process(delta: float) -> void:
+	var distance = position.distance_to(doctor.position)
+	print(distance)
+	var distance_remapped = remap(distance, 10.0, 14.0, 1.0, 0.0)
+	distance_remapped = clamp(distance_remapped, 0.0, 1.0)
+	print(distance_remapped)
+	animation_tree.set("parameters/mouth_open_blend/blend_amount", distance_remapped)
+	pass
+	
+
 #func _process(delta) -> void:
 	#marker_3d = $Marker3D
 	#body.material_overlay.set("shader_parameter/freeze_pos", marker_3d.position)
 	#pass
 
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
-		if (event.is_action_pressed("ui_action")):
-			mouth_open()
-		pass
-	pass
+
+#func _input(event: InputEvent) -> void:
+	#if event is InputEventKey:
+		#if (event.is_action_pressed("ui_action")):
+			#mouth_open()
+		#pass
+	#pass
 
 
-func mouth_open() -> void:
-	tween = get_tree().create_tween()
-	tween.tween_method(func(value): animation_tree.set("parameters/mouth_open_blend/blend_amount", value), 0.0, 1.0, GROW_TIME).set_ease(Tween.EASE_IN_OUT) # tween from current value to 1
-	pass
+#func mouth_open() -> void:
+	#tween = get_tree().create_tween()
+	#tween.tween_method(func(value): animation_tree.set("parameters/mouth_open_blend/blend_amount", value), 0.0, 1.0, GROW_TIME).set_ease(Tween.EASE_IN_OUT) # tween from current value to 1
+	#pass
 
 
 func freeze(_position_global: Vector3) -> void:
